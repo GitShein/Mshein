64f4f1a1ccc65d5e5da069a363db80bf7b2e3ff8
huchang <825662572@qq.com>
2016-07-28 15:23:38 +0800

    新建目录

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch')
+
+var DEV = '.';
+
+gulp.task('watch', function (cb) {
+    watch('sass/*.scss', function () {
+        gulp.src('sass/*.scss')
+            .pipe(sass({outputStyle: 'expanded',indentWidth:'4'}))
+            .pipe(gulp.dest('./css'))
+            .pipe(browserSync.stream());
+    });
+});
+
+gulp.task('sass', function () {
+    gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+});
+
+gulp.task('default',['watch']);
